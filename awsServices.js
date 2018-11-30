80953300c8d6a8b0cef0019b26f9e7f8f797f9f1
Salinas <lausalin@8c859016bbbe.ant.amazon.com>
2018-11-30 12:09:35 -0800

    added aws credentials popup. errors in popup.js when extension uploaded

--- awsServices.js
+++ awsServices.js
@@ -1,11 +1,60 @@
 var awsServices = (function()
 {
-	var gateway;
-})
+	var apigateway;
+
+//initGateway: function
+
+function initAWS(accessKeyId, secretAccessKey, region) //function handling credentials
+	{
+		AWS.config.region = region;
+		AWS.config.accessKeyId = accessKeyId;
+		AWS.config.secretAccessKey = secretAccessKey;
+	}
 
 function initGateway(region)
 {
-	gateway = new AWS.APIGateway ({ region: region});
+	apigateway = new AWS.APIGateway ({ region: region});
 }
 
-initGateway: function
+//define function handling url pass to api 
+function payload ()
+{
+	var params =
+	{
+
+	}
+}
+
+function initAWSServices(key, secret, region) 
+	{
+		initAWS(key, secret, region);
+	}
+
+	return { // Initialize services
+		
+		init: function(key, secret, region) 
+		{
+			initAWSServices(key, secret, region);
+		},
+	};
+});
+
+
+/* var apigClient = apigClientFactory.newClient({
+            invokeUrl: 'https://9rautkbsa5.execute-api.us-west-2.amazonaws.com/test"'
+        });
+
+var body =
+{
+	"url": "https://9rautkbsa5.execute-api.us-west-2.amazonaws.com/test",
+	"method": "POST",
+	"data": tablink
+};
+
+apigClient.chromeurl(params, body, addtionalParams) //call api method
+
+	.then(function(result){
+      // Add success callback code here.
+    }).catch( function(result){
+      // Add error callback code here.
+    });*/
--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-chrome.runtime.onInstalled.addListener(function() 
+/*chrome.runtime.onInstalled.addListener(function() 
 {
 	chrome.history.search({"text": "", "startTime": 0 , "maxResults": 1000000000}, function callback(results) 
 	{ 
@@ -6,7 +6,7 @@ chrome.runtime.onInstalled.addListener(function()
 		var json= JSON.stringify(results);
 		console.log(JSON.parse(json));
 	}) 
-});
+});*/
 
 chrome.tabs.getSelected(null, function(tab) 
 { //get current tab URL from extension
@@ -14,7 +14,9 @@ chrome.tabs.getSelected(null, function(tab)
 	var tablink = tab.url;
 });
 
-var apigClient = apigClientFactory.newClient();
+/* var apigClient = apigClientFactory.newClient({
+            invokeUrl: 'https://9rautkbsa5.execute-api.us-west-2.amazonaws.com/test"'
+        });
 
 var body =
 {
@@ -23,10 +25,48 @@ var body =
 	"data": tablink
 };
 
-apigClient.chromeurl(params, body, addtionalParams)
+apigClient.chromeurl(params, body, addtionalParams) //call api method
 
 	.then(function(result){
       // Add success callback code here.
     }).catch( function(result){
       // Add error callback code here.
-    });
+    });*/
+
+    function checkAWSCredentials(awsCredentials, callback) //require credentials check
+    {
+		awsServices.init(awsCredentials.awsAccessKeyId, awsCredentials.awsSecretAccessKey, awsCredentials.awsRegion);	
+	}
+
+	function initAWSCredentials() //validate and store credentials passed in
+	{
+		chrome.storage.sync.get('awsCredentials', function(data) 
+		{
+				var awsCredentials = data.awsCredentials;
+				
+				if (awsCredentials && awsCredentials.awsAccessKeyId !== '' && awsCredentials.awsSecretAccessKey !== '' && awsCredentials.awsRegion !== '') 
+				{
+					awsServices.init(awsCredentials.awsAccessKeyId, awsCredentials.awsSecretAccessKey, awsCredentials.awsRegion);
+					status = 'OK';
+				}
+				else
+					status = 'Not configured AWS Credentials. Please configure';
+			}
+		);
+	}
+
+	function startExtension() 
+	{
+		if (!chrome.storage) 
+		{
+			setTimeout(startExtension, 1000);
+			return;
+		}
+		initAWSCredentials();
+	}
+
+	setTimeout(startExtension, 1000);
+
+
+
+
