2f944e299e21c1960032e44d682a94f57f960fa3
Salinas <lausalin@8c859016bbbe.ant.amazon.com>
2018-12-20 14:33:23 -0800

    minor changes and code cleanup. New icons added

--- background.js
+++ background.js
@@ -1,52 +1,34 @@
-/*chrome.runtime.onInstalled.addListener(function() 
-{
-	chrome.history.search({"text": "", "startTime": 0 , "maxResults": 1000000000}, function callback(results) 
-	{ 
-		//store user history to dynamo on first install
-		var json= JSON.stringify(results);
-		console.log(JSON.parse(json));
-	}) 
-});*/
-
+//handling url pull from each active chrome tab
 chrome.tabs.onUpdated.addListener(function(id,activeInfo,tab) 
 {
 	if(activeInfo.status != ("complete"))
 		return;
-  // how to fetch tab url using activeInfo.tabid
- // chrome.tabs.get(activeInfo.tabId, function(tab)
-//  {
+
   	var tablink = tab.url;
     
     console.log(tablink.substring(0,4)); //display url on console
 
      	if (tablink.substring(0,4) == 'http')
      	{
-     		var request = new XMLHttpRequest(); // Create a request variable and assign a new XMLHttpRequest object to it.
+     		var request = new XMLHttpRequest(); // initiate http request
 
-		// Open a new connection, using the GET request on the URL endpoint
+		// Open a new connection, use GET method on the api endpoint
 		request.open('POST', 'https://3xe435ebm9.execute-api.us-east-2.amazonaws.com/Dev/classifyurl', true);
-
-		/*var temp = {"input": "{\"data\": \"https://www.cnn.com/2018/12/03/opinions/mueller-is-about-to-have-his-say-honig/index.html\"}",
-	   "stateMachineArn": "arn:aws:states:us-east-2:313327970627:stateMachine:Choicestate"}*/
 		
-		var temp = '{"data": "'+tablink+'"}'
-		//var temp2 = {"data": tablink}
-		//var temp =  {"data": "https://www.cnn.com/2018/12/03/opinions/mueller-is-about-to-have-his-say-honig/index.html"}
+		var temp = '{"data": "'+tablink+'"}' //append url in a new variable for expected format
+
 		var payload = JSON.parse(temp)
 
 		console.log(payload);
+
 		var output = request.send(temp);
-		request.onreadystatechange = function() { 
+
+		request.onreadystatechange = function() 
+		{ 
 			var out = request.responseText;
 			console.log(out);
-			document.querySelector('.classifyText').innerHTML = out;
+			document.querySelector('.classifyText').innerHTML = out; //current way to clean up text in website
 		};
 	    }
 
-
-	   /* else
-	    	console.log("error");*/
-       
-      
-  //});
 }); 
