ecdf53702f54f4de314cc402d27f00e84c4c76ae
Salinas <lausalin@8c859016bbbe.ant.amazon.com>
2018-12-24 11:31:23 -0800

    trying new method to remove elements from popup

--- popup.js
+++ popup.js
@@ -13,13 +13,15 @@ var userData =
 };
 */
 
-//use button "Login" and redirect to cognito front end
+//use button "Login" and redirect to cognito front end and delete popup contents
 function clickHandler(e) {
+
+	var element = document.getElementById('text');
+	element.parentNode.removeChild(element);
+
     chrome.tabs.create({url: "https://slant.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=68qqmdg46ulqtqffpuhjqu88ke&redirect_uri=https://slant.test.cognito.staticpage.s3-website-us-west-2.amazonaws.com/"});
-    window.close(); // Note: window.close(), not this.close()
+    window.close(); // 
 }
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('login_btn').addEventListener('click', clickHandler);
 });
-
-
