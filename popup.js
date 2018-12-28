d14e0202141cba55c95c5d890066a18acae97655
Salinas <lausalin@8c859016bbbe.ant.amazon.com>
2018-12-27 17:14:25 -0800

    new callback url, deleted old gauge.js stuff

--- popup.js
+++ popup.js
@@ -19,9 +19,38 @@ function clickHandler(e) {
 	var element = document.getElementById('text');
 	element.parentNode.removeChild(element);
 
-    chrome.tabs.create({url: "https://slant.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=68qqmdg46ulqtqffpuhjqu88ke&redirect_uri=https://slant.test.cognito.staticpage.s3-website-us-west-2.amazonaws.com/"});
+    chrome.tabs.create({url:"https://slant.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=68qqmdg46ulqtqffpuhjqu88ke&redirect_uri=https://psalabs.us"});
     window.close(); // 
 }
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('login_btn').addEventListener('click', clickHandler);
 });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.getElementById('gauge_bar').addEventListener(meter);
+// });
+
+// function meter {
+
+// 	var opts = {
+//   	angle: 0.15, /// The span of the gauge arc
+//  	lineWidth: 0.44, // The line thickness
+//   	pointer: {
+//     length: 0.9, // Relative to gauge radius
+//     strokeWidth: 0.035 // The thickness
+//   	},
+//   	colorStart: '#6FADCF',   // Colors
+//   	colorStop: '#8FC0DA',    // just experiment with them
+//   	strokeColor: '#E0E0E0'   // to see which ones work best for you
+// };
+// 	var target = document.getElementById('gauge_bar'); // your canvas element
+// 	var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+// 	gauge.maxValue = 3000; // set max gauge value
+// 	gauge.setMinValue(0);  // set min value
+// 	gauge.set(1250); // set actual value
+// }
+
+
+
+
