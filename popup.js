641532eb2100c2e9e11f92fa49a76963e58c7803
Anthony <antpas@bu.edu>
2019-01-14 16:28:44 -0800

    Fixed S3 upload

--- popup.js
+++ popup.js
@@ -24,8 +24,66 @@ function upload(){
       {
         console.log(vidReq.responseText);
         var presignedURL = vidReq.responseText;
-        var newbody = `<form id="form1" action="https://www.google.com" method="POST" class="md-form"><div class="file-field"><a class="btn-floating purple-gradient mt-0 float-left"><i class="fas fa-cloud-upload-alt"></i><input type="file"><a><div class="file-path-wrapper"><div></div></form><button type="submit" form="form1" value="Submit">Submit</button>`;
+        var newbody = `<input id="upload" type="file" />
+                         <input id="filename" type="text" />`
         document.getElementById('bodyshift').innerHTML=newbody
+        document.getElementById('upload').onchange = uploadOnChange;
+
+        function uploadOnChange() {
+            var filename = this.value;
+            var lastIndex = filename.lastIndexOf("\\");
+            if (lastIndex >= 0) {
+                filename = filename.substring(lastIndex + 1);
+            }
+
+            document.getElementById('filename').value = filename;
+            console.log(presignedURL);
+            //presignedURL = '<%=@Url.Action("") %>'
+
+            $.ajax({
+                url: presignedURL, //presinged-url which you get from server side
+                type: 'POST',
+                data: filename,
+                processData: false,
+                contentType: false,
+                headers: {'Content-Type': 'multipart/form-data'},
+                success: function(result){
+                    console.log(result);
+                }
+                
+            });
+        }
+        
+        var oldbody = 
+        `<html>
+          <head>
+            
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            
+          </head>
+          <body>
+
+          <form  action="https://s3.us-east-2.amazonaws.com/youtubedownloadsslant/" method="post" enctype="multipart/form-data">
+            Key to upload: 
+            <input type="input"  name="key" value="antpas/hi.jpg" /><br />
+            <input type="hidden" name="success_action_redirect" value="http://www.psalabs.us" />
+            Content-Type: 
+            <input type="input"  name="Content-Type" value="image/jpeg" /><br />
+            <input type="hidden" name="x-amz-server-side-encryption" value="AES256" /> 
+            <input type="text"   name="X-Amz-Credential" value="AKIAJZSV6TCY3Q6532LA/20190114/us-east-2/s3/aws4_request" />
+            <input type="text"   name="X-Amz-Algorithm" value="AWS4-HMAC-SHA256" />
+            <input type="text"   name="X-Amz-Date" value="20190114T222019Z" />
+
+            Tags for File: 
+            <input type="input"  name="x-amz-meta-tag" value="" /><br />
+            <input type="hidden" name="Policy" value="eyJleHBpcmF0aW9uIjoiMjAxOS0wMS0xNFQyMzoyMDoxOVoiLCJjb25kaXRpb25zIjpbeyJrZXkiOiJhbnRwYXMvaGkuanBnIn0seyJidWNrZXQiOiJ5b3V0dWJlZG93bmxvYWRzc2xhbnQifSx7IlgtQW16LUFsZ29yaXRobSI6IkFXUzQtSE1BQy1TSEEyNTYifSx7IlgtQW16LUNyZWRlbnRpYWwiOiJBS0lBSlpTVjZUQ1kzUTY1MzJMQS8yMDE5MDExNC91cy1lYXN0LTIvczMvYXdzNF9yZXF1ZXN0In0seyJYLUFtei1EYXRlIjoiMjAxOTAxMTRUMjIyMDE5WiJ9XX0" />
+            <input type="hidden" name="X-Amz-Signature" value="381f5640c4aa30a7f501624748496f00b0bd05df4be496e198897688666b1a4b" />
+            File: 
+            <input type="file"   name="file" /> <br />
+            <!-- The elements after this will be ignored -->
+            <input type="submit" name="submit" value="Upload to Amazon S3" />
+          </form>
+        </html>`
       }
     });
 };
@@ -52,6 +110,14 @@ function overall(){
     
 };
 
+// function biasview(){
+
+//        chrome.storage.local.get(apiReturn, function){
+//         //display on document.getElementById("SlantPopup") is CLICKED
+//         //MOE FILL IN UR MAGIC HERE
+//      }
+// }
+
 var port = chrome.extension.connect({
    name: "Bias Data"
 });
@@ -76,7 +142,10 @@ port.onMessage.addListener(function(msg) {
      });
 });
 
-chrome.storage.local.get(['party'], function(result) {});
+chrome.storage.local.get(['party'], function(result) {
+  //var y = document.getElementById("SlantPopup2");
+  //y.querySelector('.partyforslant').innerHTML = result.party;
+});
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('login_btn').addEventListener('click', clickHandler);
